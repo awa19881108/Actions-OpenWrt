7a3683fabfab23447dc43b34bc21bfb55ee20daa
awa19881108 <awa19881108@gmail.com>
2026-02-03 03:24:56 +0800

    Create dummy.sh

---
+++ dummy.sh
@@ -0,0 +1,8 @@
+env:
+  REPO_URL: https://github.com/openwrt/openwrt
+  REPO_BRANCH: v24.10.5
+  FEEDS_CONF: feeds.conf.default
+  CONFIG_FILE: openwrt.config
+  DIY_P1_SH: customize.sh
+  TZ: Asia/Shanghai
+  DIY_P2_SH: dummy.sh  # 已配置，确认存在即可
